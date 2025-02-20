1bc36ad0607ba570d3d2ea62aa71dfd5c7b4c8d6
Kristaps Dzenis (dzenisk) <dzenisk@coventry.ac.uk>
2025-02-20 17:21:18 +0000

    Added organisation creation form, database table. Added image upload and storage in database. Other small updates to account creation page

--- SQL.py
+++ SQL.py
@@ -19,6 +19,15 @@ def check_db():
     profile_image BLOB
     );""")
 
+    cursor.execute("""CREATE TABLE IF NOT EXISTS org_details(
+        username TEXT PRIMARY KEY,
+        password TEXT NOT NULL,
+        org_school_name TEXT NOT NULL,
+        e_mail TEXT NOT NULL,
+        account_type TEXT NOT NULL,
+        profile_image BLOB
+        );""")
+
     connect_db.commit()  # commit changes to database
     connect_db.close()  # close database
 
@@ -28,7 +37,7 @@ def insert_user(username, password, email, firstName, lastName, Org_School, acc_
     connect_db = sqlite3.connect('database.db')  # connect to database
     cursor = connect_db.cursor()
 
-    cursor.execute("INSERT INTO user_details VALUES (:username, :password, :fname, :sname, :org_school_name, "
+    cursor.execute("INSERT OR IGNORE INTO user_details VALUES (:username, :password, :fname, :sname, :org_school_name, "
                    ":e_mail, :account_type, :id, :profile_image)", {
         "username": username,
         "password": password,
@@ -38,6 +47,25 @@ def insert_user(username, password, email, firstName, lastName, Org_School, acc_
         "e_mail": email,
         "account_type": acc_Type,
         "id": ID,
+        "profile_image": image
+    })
+
+    connect_db.commit()  # commit changes to database
+    connect_db.close()  # close database
+
+
+# function to insert new organisation data into org_details table
+def insert_org(username, password, email, Org_School, acc_Type, image) :
+    connect_db = sqlite3.connect('database.db')  # connect to database
+    cursor = connect_db.cursor()
+
+    cursor.execute("INSERT OR IGNORE INTO org_details VALUES (:username, :password, :org_school_name, "
+                   ":e_mail, :account_type, :profile_image)", {
+        "username": username,
+        "password": password,
+        "org_school_name": Org_School,
+        "e_mail": email,
+        "account_type": acc_Type,
         "profile_image": image
     })
 
